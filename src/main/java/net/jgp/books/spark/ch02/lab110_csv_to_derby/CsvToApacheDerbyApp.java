8988cf8d7aaaa5d1fd9b2c4338163fa82884edde
Christoph Schuetz <christoph.schuetz@jku.at>
2020-01-20 09:42:38 +0100

    some fixes and use derby when exec

--- src/main/java/net/jgp/books/spark/ch02/lab110_csv_to_derby/CsvToApacheDerbyApp.java
+++ src/main/java/net/jgp/books/spark/ch02/lab110_csv_to_derby/CsvToApacheDerbyApp.java
@@ -7,10 +7,8 @@ import java.net.InetAddress;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Properties;
 
 import org.apache.derby.drda.NetworkServerControl;
